a013ba6917058e4845c673ce80675b01819077b0
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2025-05-03 15:13:11 -0300

    fix: descrição

--- src/academia/Exercicio.java
+++ src/academia/Exercicio.java
@@ -2,23 +2,17 @@ package academia;
 
 public class Exercicio {
     private String nome;
-    private String descricao;
 
-    public Exercicio(String nome, String descricao) {
+    public Exercicio(String nome) {
         this.nome = nome;
-        this.descricao = descricao;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
     @Override
     public String toString() {
-        return "Exercicio{nome='" + nome + "', descricao='" + descricao + "'}";
+        return "Exercicio{nome='" + nome + "'}";
     }
 }
